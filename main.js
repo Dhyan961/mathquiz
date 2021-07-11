8ddd8ae4717bdf15b717443e3d859a844cf9513e
Dhyan961 <79209452+Dhyan961@users.noreply.github.com>
2021-07-11 10:46:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function addUser(){
+    player1 = document.getElementById("player1_name_input").value;
+    player2 = document.getElementById("player2_name_input").value;
+
+    localStorage.setItem("Player1" , player1);
+    localStorage.setItem("Player2" , player2);
+
+    window.location = "2index.html";
+}
